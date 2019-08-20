1a3aa1b6a49b2e93aad227c5f5f6222d40326612
iostrovskii1976 <crion2005@yandex.ru>
2019-08-20 10:55:00 +0300

    Создан класс описания модели БД

---
+++ WebWorkflow/WebWorkflow/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebWorkflow.ModelBD
+{
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Pass { get; set; }
+        public int GroupsUsersId { get; set; }
+    }
+
+    public class GroupUsers
+    {
+        public int Id { get; set; }
+        public string GroupName { get; set; }
+    }
+
+    public class Document
+    {
+        public string DocumentType { get; set; }
+        public DateTime Date { get; set; }
+        public int UsersId { get; set; }
+    }
+
+    public class Folders
+    {
+        public int Id { get; set; }
+        public string NameFolder { get; set; }
+    }
+
+    public class DocumentVersion
+    {
+        public int Id { get; set; }
+        public byte[] File { get; set; }
+        public int UsersId { get; set; }
+    }
+
+    public class PermissionFolders
+    {
+        public int FoldersId { get; set; }
+        public string AccessLevel { get; set; }
+        public int GroupsUsersId { get; set; }
+        public int UsersId { get; set; }
+    }
+}
